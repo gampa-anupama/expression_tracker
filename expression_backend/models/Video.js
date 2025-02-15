3f0699d3baae319b90e64e4821af8206efa73a88
gampa-anupama <anupamagampa1@gmail.com>
2025-02-15 23:55:09 +0530

    Delete expression_backend directory

--- expression_backend/models/Video.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const VideoSchema = new mongoose.Schema({
-  filename: String,
-  filepath: String,
-});
-
-const Video = mongoose.model("Video", VideoSchema);
-module.exports = Video;
--- expression_backend/models/videoModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const videoSchema = new mongoose.Schema({
-  filename: { type: String, required: true },
-  fileUrl: { type: String, required: true },
-  uploadedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model("Video", videoSchema);
